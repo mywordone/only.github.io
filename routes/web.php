c52454c537d07116f96b28c5338c0a9b9fffe4a3
有心沉默无心快语 <914040633@qq.com>
2018-08-16 11:30:42 +0800

    add

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('users', function() {
+    return "在水一方";
 });
