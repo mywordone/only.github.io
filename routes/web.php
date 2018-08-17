2879b2f9b27d9c264ec87dca57f3466b3d6f592e
sukang120482 <18234132853@163.com>
2018-08-17 14:14:48 +0800

    web.php

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@
 
 
 
-<<<<<<< HEAD
+
 //后台首页登录
 Route::get('admin/public/login', 'Admin\PublicController@login') -> name('admin_login');
 Route::get('admin/public/check', 'Admin\PublicController@check') -> name('admin_check');
@@ -22,19 +22,9 @@ Route::group(['prefix' => 'admin'], function (){
 	Route::any('matchdata/import', 'admin\MatchDataController@import')->name('matchdata_import');
 	Route::get('matchdata/export', 'admin\MatchDataController@export')->name('template_export');
 	Route::get('matchdata/index', 'admin\MatchDataController@index')->name('matchdata_index');
-=======
-
-//登录首页
-Route::get('admin/public/login', 'Admin\PublicController@login') -> name('admin_login');
-Route::post('admin/public/check', 'Admin\PublicController@check') -> name('admin_check');
-
-Route::group(['prefix' => 'admin'],function (){
-    //登录后台首页
-    Route::get('index/index','admin\IndexController@index')->name('index_index');
+	Route::get('index/index','admin\IndexController@index')->name('index_index');
     Route::get('index/welcome','admin\IndexController@welcome')->name('index_welcome');
 
-    //
->>>>>>> 06feaf2db83f9ae729c65e7eb658c6764d19c39c
 });
 
 
